cd9873f1906934f38e7874dd8e5aa7d1b20f5637
nilofarramzani83-wq <nilofar.ramzani83@gmail.com>
2026-08-18 08:42:38 +0330

    Delete functions/api directory

--- functions/api/logout.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestPost() {
-  const headers = new Headers({ "content-type": "application/json; charset=utf-8" });
-  headers.append("Set-Cookie", "pa_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0");
-  return new Response(JSON.stringify({ ok: true }), { status: 200, headers });
-}
- 
